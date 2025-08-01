6f0842a95664c5353c8707a75716d07072da6e4b
Borislav Castillo Krasteva <bor.castillo.k@saintcharlescollege.cl>
2025-08-01 19:26:09 -0400

    socket format change

--- src/socket.cpp
+++ src/socket.cpp
@@ -5,12 +5,10 @@
 // [packet manual](https://linux.die.net/man/7/packet)
 int open_socket() { return socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL)); }
 #elif defined(__APPLE__)
+#include <fcntl.h>
 #include <net/bpf.h>
 #include <sys/socket.h>
-#include <fcntl.h>
-int open_socket() {
-  return open("/dev/bpf0", O_RDWR);
-}
+int open_socket() { return open("/dev/bpf0", O_RDWR); }
 #else
 #error "unsupported platform: only availabe on linux and macos"
 #endif
