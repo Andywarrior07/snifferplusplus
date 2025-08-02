9f6edbeca057fdd9107a9a6f04e4b78a6322f895
Andy Guerrero <andre.guerrerocab@gmail.com>
2025-08-01 21:24:56 -0400

    create socket class to open raw socket

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,20 @@
 #include <cstring>
 #include <iostream>
 #include <ostream>
+#include <ifaddrs.h>
+#include <net/if.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 int main() {
     // Step 1: open a socket
-    int socket_file_descriptor = open_socket();
+    Socket socket;
+
+    if (!socket.initialize()) {
+        return -1;
+    }
+
+    const int socket_file_descriptor = socket.get_socket();
 
     if (socket_file_descriptor == -1) {
         std::cerr << "Error while creating file descriptor: "
@@ -14,9 +24,38 @@ int main() {
         return -1;
     }
     // Step 2: inspect NICs
+    ifaddrs *ifaddr;
+
+    if (getifaddrs(&ifaddr) == -1) {
+        std::cerr << "Error while getting NICs: "
+            << std::strerror(errno) << std::endl;
+        return -1;
+    }
+
+    for (const ifaddrs* ifa = ifaddr; ifa != nullptr; ifa = ifa->ifa_next) {
+        if (ifa->ifa_addr == nullptr) continue;
+
+        std::cout << "Interface: " << ifa->ifa_name << "\n";
+
+        if (ifa->ifa_addr->sa_family == AF_INET) {
+            char ip[INET_ADDRSTRLEN];
+            const sockaddr_in *sa = reinterpret_cast<struct sockaddr_in*>(ifa->ifa_addr);
+            inet_ntop(AF_INET, &(sa->sin_addr), ip, INET_ADDRSTRLEN);
+            std::cout << "  IPv4 Address: " << ip << "\n";
+
+
+        } else if (ifa->ifa_addr->sa_family == AF_INET6) {
+            char ip[INET6_ADDRSTRLEN];
+            const sockaddr_in6 *sa = reinterpret_cast<struct sockaddr_in6*>(ifa->ifa_addr);
+            inet_ntop(AF_INET6, &(sa->sin6_addr), ip, INET6_ADDRSTRLEN);
+            std::cout << "  IPv6 Address: " << ip << "\n";
+        }
+    }
+
 
     // Step 3: bind to NIC
 
     // Step 4: recieve packages
+    freeifaddrs(ifaddr);
     return 0;
 }
--- src/socket.cpp
+++ src/socket.cpp
@@ -1,13 +1,54 @@
+#include <iostream>
 #if defined(__linux__)
 #include <linux/if_ether.h>
 #include <netinet/in.h>
 // [socket manual](https://linux.die.net/man/7/socket)
 // [packet manual](https://linux.die.net/man/7/packet)
-int open_socket() { return socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL)); }
 #elif defined(__APPLE__)
 #include <fcntl.h>
 #include <sys/socket.h>
-int open_socket() { return open("/dev/bpf0", O_RDWR); }
 #else
 #error "unsupported platform: only availabe on linux and macos"
 #endif
+
+class Socket {
+public:
+    bool initialize() {
+#ifdef __linux__
+        raw_socket = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+#elif __APPLE__
+        raw_socket = open_bpf_socket();
+#endif
+        if (raw_socket == -1) {
+            std::cerr << "Error while creating file descriptor: "
+                << std::strerror(errno) << std::endl;
+            return false;
+        };
+
+        return true;
+    }
+
+    int get_socket() const { return raw_socket; }
+
+private:
+    int raw_socket = -1;
+
+#ifdef __APPLE__
+    static int open_bpf_socket() {
+        int socket = -1;
+        char bpf_device[12];
+
+        for (int i = 0; i < 255; i++) {
+            snprintf(bpf_device, sizeof(bpf_device), "/dev/bpf%d", i);
+            socket = open(bpf_device, O_RDWR);
+
+            if (socket != -1) {
+                std::cout << "Using bpf: " << bpf_device << std::endl;
+                return socket;
+            }
+        }
+
+        return -1;
+    }
+#endif
+};
