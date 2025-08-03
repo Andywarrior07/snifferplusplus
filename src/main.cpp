d7b15c3a6f6399a036d474d51022e71dc5e77fc5
Borislav Castillo Krasteva <bor.castillo.k@saintcharlescollege.cl>
2025-08-02 20:45:13 -0400

    🦆 Replaced gravity with ducks. Everything still falls, but now it's funnier.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
-#include "socket.cpp"
-#include <cerrno>
-#include <cstring>
-#include <iostream>
-#include <ostream>
+#include <arpa/inet.h>
 #include <ifaddrs.h>
 #include <net/if.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
+
+#include <cstring>
+#include <iostream>
+#include <memory>
+#include <ostream>
+
+#include "socket.cpp"
 
 int main() {
     // Step 1: open a socket
@@ -18,42 +20,14 @@ int main() {
 
     const int socket_file_descriptor = socket.get_socket();
 
-    if (socket_file_descriptor == -1) {
-        std::cerr << "Error while creating file descriptor: "
-            << std::strerror(errno) << std::endl;
-        return -1;
-    }
     // Step 2: inspect NICs
-    ifaddrs *ifaddr;
-
-    if (getifaddrs(&ifaddr) == -1) {
-        std::cerr << "Error while getting NICs: "
-            << std::strerror(errno) << std::endl;
-        return -1;
-    }
-
-    for (const ifaddrs* ifa = ifaddr; ifa != nullptr; ifa = ifa->ifa_next) {
-        if (ifa->ifa_addr == nullptr) continue;
-
-        std::cout << "Interface: " << ifa->ifa_name << "\n";
-
-        if (ifa->ifa_addr->sa_family == AF_INET) {
-            char ip[INET_ADDRSTRLEN];
-            const sockaddr_in *sa = reinterpret_cast<struct sockaddr_in*>(ifa->ifa_addr);
-            inet_ntop(AF_INET, &(sa->sin_addr), ip, INET_ADDRSTRLEN);
-            std::cout << "  IPv4 Address: " << ip << "\n";
-
-
-        } else if (ifa->ifa_addr->sa_family == AF_INET6) {
-            char ip[INET6_ADDRSTRLEN];
-            const sockaddr_in6 *sa = reinterpret_cast<struct sockaddr_in6*>(ifa->ifa_addr);
-            inet_ntop(AF_INET6, &(sa->sin6_addr), ip, INET6_ADDRSTRLEN);
-            std::cout << "  IPv6 Address: " << ip << "\n";
-        }
-    }
-
+    ifaddrs* ifaddr = get_network_interfaces();
 
     // Step 3: bind to NIC
+    std::string nic_selected;
+
+    std::cout << "Select a network interface\n";
+    std::cin >> nic_selected;
 
     // Step 4: recieve packages
     freeifaddrs(ifaddr);
--- src/socket.cpp
+++ src/socket.cpp
@@ -1,10 +1,18 @@
+#include <arpa/inet.h>
+#include <ifaddrs.h>
+
+#include <array>
+#include <cstdlib>
+#include <cstring>
 #include <iostream>
-#if defined(__linux__)
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#ifdef __linux__
 #include <linux/if_ether.h>
 #include <netinet/in.h>
-// [socket manual](https://linux.die.net/man/7/socket)
-// [packet manual](https://linux.die.net/man/7/packet)
-#elif defined(__APPLE__)
+#elif __APPLE__
 #include <fcntl.h>
 #include <sys/socket.h>
 #else
@@ -12,25 +20,28 @@
 #endif
 
 class Socket {
-public:
+   public:
     bool initialize() {
 #ifdef __linux__
+        // [socket manual](https://linux.die.net/man/7/socket)
+        // [packet manual](https://linux.die.net/man/7/packet)
         raw_socket = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
 #elif __APPLE__
         raw_socket = open_bpf_socket();
 #endif
         if (raw_socket == -1) {
-            std::cerr << "Error while creating file descriptor: "
-                << std::strerror(errno) << std::endl;
+            std::cerr << "Error while creating file descriptor: " << std::strerror(errno) << std::endl;
             return false;
         };
 
         return true;
     }
 
-    int get_socket() const { return raw_socket; }
+    // Platform dependent. Creates an unbound file descriptor that we will
+    // connect to a Network Interface.
+    [[nodiscard]] int get_socket() const { return raw_socket; }
 
-private:
+   private:
     int raw_socket = -1;
 
 #ifdef __APPLE__
@@ -52,3 +63,31 @@ private:
     }
 #endif
 };
+
+ifaddrs* get_network_interfaces() {
+    ifaddrs* ifaddr;
+
+    if (getifaddrs(&ifaddr) == -1) {
+        std::cerr << "Error while getting NICs: " << std::strerror(errno) << std::endl;
+        exit(1);
+    }
+
+    std::vector<std::string> nics;
+    std::unordered_set<std::string> viewed;
+
+    for (const ifaddrs* ifa = ifaddr; ifa != nullptr; ifa = ifa->ifa_next) {
+        if (ifa->ifa_addr == nullptr) {
+            continue;
+        }
+
+        if (viewed.insert(ifa->ifa_name).second) {
+            nics.push_back(ifa->ifa_name);
+        }
+    }
+
+    for (const auto& name : nics) {
+        std::cout << "Name: " << name << std::endl;
+    }
+
+    return ifaddr;
+}
