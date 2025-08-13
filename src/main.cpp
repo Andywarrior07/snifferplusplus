bc1363869a1c45dfd9eb8193a10b9977ea71ff0a
Borislav Castillo Krasteva <bor.castillo.k@saintcharlescollege.cl>
2025-08-12 21:45:58 -0400

    main cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
+#include <ifaddrs.h>
+#include <string.h>
+
 #include <iostream>
 #include <unordered_set>
-#include <ifaddrs.h>
-#include "Packet.hpp"
+
+// #include "Packet.hpp"
 #include "socket/RawSocket.h"
 
 std::string get_user_input(const std::vector<std::string>& nic_names);
@@ -9,10 +12,10 @@ std::vector<std::string> get_network_interfaces();
 
 int main() {
     // Step 1: inspect network interfaces.
-    std::vector<std::string> nic_names = get_network_interfaces();
+    vector<string> nic_names = get_network_interfaces();
 
     // Step 2: prompt the user to pick an interface.
-    std::string selected_nic = get_user_input(nic_names);
+    string selected_nic = get_user_input(nic_names);
 
     // Step 3: open a socket
 
@@ -22,71 +25,60 @@ int main() {
         return -1;
     }
 
-    const auto packet = std::make_unique<Packet>();
-    const auto buffer = std::make_unique<uint8_t[]>(4096);
-
-    while (true) {
-        const ssize_t data_size = socket.read_packet(buffer.get());
-
-        if (data_size < 0) {
-            std::cerr << "Error reading packet: " << std::strerror(errno) << std::endl;
-            break;
-        }
-
-        if (data_size == 0) {
-            continue; // No hay datos, continuar
-        }
-
-
-        packet->process_bpf_buffer(buffer.get(), static_cast<size_t>(data_size));
-    }
-
-    // Step 3: bind to NIC
-
     // Step 4: recieve packages
     return 0;
 }
 
-std::vector<std::string> get_network_interfaces() {
-    ifaddrs* ifaddr;
+// Inspect Network Interfaces using `getifaddrs()`
+vector<string> get_network_interfaces() {
+    // Linked list containing a chain of `ifaddrs`
+    ifaddrs* interface_address;
 
-    if (getifaddrs(&ifaddr) == -1) {
-        std::cerr << "Error while getting NICs: " << std::strerror(errno) << std::endl;
+    // Use `getifaddrs()` to populate out `interface_address` linker list
+    if (getifaddrs(&interface_address) == -1) {
+        std::cerr << "Error while getting NICs: " << strerror(errno) << std::endl;
         exit(1);
     }
 
-    unordered_set<std::string> names;
-
-    for (const ifaddrs* ifa = ifaddr; ifa != nullptr; ifa = ifa->ifa_next) {
+    // Iterate over linked list and store names of NICs in set to dedupe.
+    unordered_set<string> ni_names;
+    for (const ifaddrs* ifa = interface_address; ifa != nullptr; ifa = ifa->ifa_next) {
         if (ifa->ifa_addr == nullptr) {
             continue;
         }
 
-        names.insert(ifa->ifa_name);
+        ni_names.insert(ifa->ifa_name);
     }
-    freeifaddrs(ifaddr);
 
-    std::vector<std::string> unique_names(names.begin(), names.end());
+    // Call destructor, since we already copied the names
+    freeifaddrs(interface_address);
+
+    // Create vector from set for ease of iteration
+    vector<string> unique_names(ni_names.begin(), ni_names.end());
     return unique_names;
 }
 
 // Prompts the user to select a NIC.
 // Returns a valid name or exits with status 1.
-std::string get_user_input(const std::vector<std::string>& nic_names) {
+string get_user_input(const vector<std::string>& nic_names) {
     std::cout << "Select a network interface:\n\n";
+
+    // Start index at 1 because Lua
     int index = 1;
     for (const auto& name : nic_names) {
         std::cout << index++ << ": " << name << std::endl;
     }
     std::cout << std::endl;
 
-    unsigned long nic_idx;
+    // Save user selection and cast to uint64
+    size_t nic_idx;
     std::cin >> nic_idx;
 
-    if (nic_idx < 1 || nic_idx > (unsigned long) nic_names.size()) {
+    if (nic_idx < 1 || nic_idx > nic_names.size()) {
         std::cerr << "unknown network interface\n";
         exit(1);
     }
 
+    // Account for Lua index
     return nic_names[nic_idx - 1];
 }
